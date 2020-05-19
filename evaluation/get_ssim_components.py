856bc70e1c3e4738b5ab09af0f8badcd3c7e2e22
eduarddohr <eduarddohr@gmail.com>
2020-05-19 17:52:48 +0300

    eval branch

--- evaluation/get_ssim_components.py
+++ evaluation/get_ssim_components.py
@@ -41,10 +41,10 @@ if __name__ == '__main__':
 
     split_file = args.split_file
     js = get_ply(split_file)
-    b_ssim = []
-    b_lumi = []
-    b_cont = []
-    b_stru = []
+    # b_ssim = []
+    # b_lumi = []
+    # b_cont = []
+    # b_stru = []
     p_ssim = []
     p_lumi = []
     p_cont = []
@@ -63,36 +63,36 @@ if __name__ == '__main__':
             gt_path = os.path.join(gt_path_main, tag)
             pred_path = os.path.join(pred_path_main, tag)
             gt = None
-            bl = None
+            # bl = None
             pc = None
             for i in range(20):
-                gt_filename = os.path.join(gt_path, 'render_{}_128.png'.format(i))
-                blender_filename = os.path.join(pred_path, 'blender_render_{}_128.png'.format(i))
+                gt_filename = os.path.join(gt_path, 'render_{}_64.png'.format(i))
+                # blender_filename = os.path.join(pred_path, 'blender_render_{}_128.png'.format(i))
                 pc2pix_filename = os.path.join(pred_path, 'pc2pix_render_{}_128.png'.format(i))
 
-                gt_im = scipy.misc.imread(gt_filename)
+                gt_im = scipy.misc.imread(gt_filename, mode='RGB')
                 w = gt_im.shape[0]
                 h = gt_im.shape[1]
                 c = gt_im.shape[2]
                 gt_im = np.reshape(gt_im, [1, w, h, c])
-                bl_im = scipy.misc.imread(blender_filename)
-                bl_im = np.reshape(bl_im, [1, w, h, c])
+                # bl_im = scipy.misc.imread(blender_filename)
+                # bl_im = np.reshape(bl_im, [1, w, h, c])
                 pc_im = scipy.misc.imread(pc2pix_filename)
                 pc_im = np.reshape(pc_im, [1, w, h, c])
                 if gt is None:
                     gt = np.array(gt_im)
-                    bl = np.array(bl_im)
+                    # bl = np.array(bl_im)
                     pc = np.array(pc_im)
                 else:
                     gt = np.append(gt, gt_im, axis=0)
-                    bl = np.append(bl, bl_im, axis=0)
+                    # bl = np.append(bl, bl_im, axis=0)
                     pc = np.append(pc, pc_im, axis=0)
 
-            l, c, s, _ssim = ssim(gt, bl)
-            b_ssim = np.append(b_ssim, _ssim)
-            b_lumi = np.append(b_lumi, l)
-            b_cont = np.append(b_cont, c)
-            b_stru = np.append(b_stru, s)
+            # l, c, s, _ssim = ssim(gt, bl)
+            # b_ssim = np.append(b_ssim, _ssim)
+            # b_lumi = np.append(b_lumi, l)
+            # b_cont = np.append(b_cont, c)
+            # b_stru = np.append(b_stru, s)
 
             l, c, s, _ssim = ssim(gt, pc)
             p_ssim = np.append(p_ssim, _ssim)
@@ -102,9 +102,9 @@ if __name__ == '__main__':
 
             t += 1
             elapsed_time = datetime.datetime.now() - start_time
-            print(str(t), "/", test_len, ": ", blender_filename, pc2pix_filename, "Elapsed :", elapsed_time)
-            print("b_ssim:", np.mean(b_ssim), "p_ssim:", np.mean(p_ssim))
-            print("b_lumi:", np.mean(b_lumi), "p_lumi:", np.mean(p_lumi))
-            print("b_cont:", np.mean(b_cont), "p_cont:", np.mean(p_cont))
-            print("b_stru:", np.mean(b_stru), "p_stru:", np.mean(p_stru))
+            print(str(t), "/", test_len, ": ", pc2pix_filename, "Elapsed :", elapsed_time)
+            print("p_ssim:", np.mean(p_ssim))
+            print("p_lumi:", np.mean(p_lumi))
+            print("p_cont:", np.mean(p_cont))
+            print("p_stru:", np.mean(p_stru))
 
