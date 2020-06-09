d2cd7dba2cd1317998f7b2fd3c485554cd9ba728
eduarddohr <eduarddohr@gmail.com>
2020-06-09 20:33:42 +0300

    no message

--- pc2pix.py
+++ pc2pix.py
@@ -160,7 +160,7 @@ class PC2Pix():
         print("test_azim_code min: ", np.amin(test_azim_code), " test_azim_code max: ", np.amax(test_azim_code))
         print("batch_size: ", self.batch_size, " pc_code_dim: ", self.pc_code_dim)
         print("Color images: ", self.color)
-        option_file = open("weights/opt.txt", "r")
+        option_file = open("/content/drive/My Drive/Licenta/Dohr/saved_weights/opt.txt", "r")
         steps_done = option_file.readline()
         # total_time_spent = option_file.readline()
         # total_time_spent = total_time_spent.split('.')
@@ -243,7 +243,7 @@ class PC2Pix():
                     prefix += "-gray"
                 if self.gen_spectral_normalization:
                     prefix += "-sn"
-                fname = os.path.join("weights", prefix + ".h5")
+                fname = "/content/drive/My Drive/Licenta/Dohr/saved_weights/" + prefix + ".h5"
                 self.generator_single.save_weights(fname)
                 prefix = self.category + "-dis"
                 if self.color:
@@ -252,23 +252,23 @@ class PC2Pix():
                     prefix += "-gray"
                 if self.gen_spectral_normalization:
                     prefix += "-sn"
-                fname = os.path.join("weights", prefix + ".h5")
+                fname = "/content/drive/My Drive/Licenta/Dohr/saved_weights/" + prefix + ".h5"
                 self.discriminator_single.save_weights(fname)
 
-                option_file = open("weights/opt.txt", "w")
+                option_file = open("/content/drive/My Drive/Licenta/Dohr/saved_weights/opt.txt", "w")
                 option_file.write(str(step))
                 option_file.close()
 
-                history_file = open("weights/history.txt", "a")
+                history_file = open("/content/drive/My Drive/Licenta/Dohr/saved_weights/history.txt", "a")
                 history_file.write(log + "\n")
                 history_file.close()
-            if (step + 1) % (save_interval * 4) == 0:
-                if os.path.isdir("/content/drive/My Drive/Licenta/Dohr/saved_weights/backup/resnet50/resnet50_" + str(step)) == False:
-                    os.mkdir("/content/drive/My Drive/Licenta/Dohr/saved_weights/resnet50/backup/resnet50_" + str(step))
-                self.generator_single.save_weights(
-                    "/content/drive/My Drive/Licenta/Dohr/saved_weights/backup/resnet50/resnet50_" + str(step) + "/chair-gen-color-" + str(step) + ".h5")
-                self.discriminator_single.save_weights(
-                    "/content/drive/My Drive/Licenta/Dohr/saved_weights/backup/resnet50/resnet50_" + str(step) + "/chair-dis-color-" + str(step) + ".h5")
+            # if (step + 1) % (save_interval * 4) == 0:
+            #     if os.path.isdir("/content/drive/My Drive/Licenta/Dohr/saved_weights/backup/resnet50/resnet50_" + str(step)) == False:
+            #         os.mkdir("/content/drive/My Drive/Licenta/Dohr/saved_weights/resnet50/backup/resnet50_" + str(step))
+            #     self.generator_single.save_weights(
+            #         "/content/drive/My Drive/Licenta/Dohr/saved_weights/backup/resnet50/resnet50_" + str(step) + "/chair-gen-color-" + str(step) + ".h5")
+            #     self.discriminator_single.save_weights(
+            #         "/content/drive/My Drive/Licenta/Dohr/saved_weights/backup/resnet50/resnet50_" + str(step) + "/chair-dis-color-" + str(step) + ".h5")
 
 
     def azim_loss(self, y_true, y_pred):
